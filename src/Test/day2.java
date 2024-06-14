051378644997c41a75c8d24ad4d9e8ca580084f8
Hariprasad <prasadvpp1@gmail.com>
2024-06-14 11:38:11 +0530

    this commit from develop branch gitdemok

--- src/Test/day2.java
+++ src/Test/day2.java
@@ -8,11 +8,17 @@ public class day2 {
 	@Test(groups = { "smoke" })
 	public void ploan() {
 		System.out.println("Good");
+		//commit from gitdemok
+		System.out.println("Fine");
+		System.out.println("Bad");
 	}
 
 	@BeforeTest
 	public void prerequiste() {
 		System.out.println("It will execute first");
+		//commit from gitdemok
+		System.out.println("First");
+		System.out.println("Second");
 	}
 
 }
