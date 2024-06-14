8bad2d2a3b064ed17937d5074dfa1bc75a457071
Hariprasad <prasadvpp1@gmail.com>
2024-06-14 15:35:43 +0530

    this commit from develop branch gitstuff

--- src/Test/day2.java
+++ src/Test/day2.java
@@ -8,13 +8,13 @@ public class day2 {
 	@Test(groups = { "smoke" })
 	public void ploan() {
 		System.out.println("Good");
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		
-=======
+//=======
 		//commit from gitdemok
 		System.out.println("Fine");
 		System.out.println("Bad");
->>>>>>> 051378644997c41a75c8d24ad4d9e8ca580084f8
+//>>>>>>> 051378644997c41a75c8d24ad4d9e8ca580084f8
 	}
 
 	@BeforeTest
